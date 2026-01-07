f3963df384e5208780fa4be002d21f00fe503c70
BlazeShotS <abelpaulo.durandpalacios21@gmail.com>
2026-01-07 12:44:39 -0500

    init: inicializando proyecto

---
+++ src/main/java/com/sistemapos/sistematextil/SistematextilApplication.java
@@ -0,0 +1,13 @@
+package com.sistemapos.sistematextil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistematextilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistematextilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sistemapos/sistematextil/SistematextilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistemapos.sistematextil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistematextilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

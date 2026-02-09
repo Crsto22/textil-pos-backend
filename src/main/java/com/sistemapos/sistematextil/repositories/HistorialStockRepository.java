1a733b5b4ef33a9a4a8a8e6051a6f3ba91e0999f
bryantem2002 <bryantem565@gmail.com>
2026-02-08 19:32:46 -0500

    wip: cambios en configuracion

---
+++ src/main/java/com/sistemapos/sistematextil/repositories/HistorialStockRepository.java
@@ -0,0 +1,18 @@
+package com.sistemapos.sistematextil.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.sistemapos.sistematextil.model.HistorialStock;
+
+@Repository
+public interface HistorialStockRepository extends JpaRepository<HistorialStock, Integer> {
+    
+    // Buscar historial de un producto específico ordenado por lo más reciente
+    List<HistorialStock> findByProductoIdProductoOrderByFechaDesc(Integer idProducto);
+    
+    // Buscar movimientos realizados en una sucursal específica
+    List<HistorialStock> findBySucursalIdSucursalOrderByFechaDesc(Integer idSucursal);
+}
